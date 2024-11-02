16e9db7a026b146e5c476ab63e21fa8076ca23d9
asg1998f <asg1998f@gmail.com>
2024-11-02 17:33:39 +0100

    elementos de html a js

---
+++ main.js
@@ -0,0 +1,7 @@
+const botonEmpezar = document.querySelector("#btn-inicio")
+const botonSiguiente = document.querySelector("#btn-siguiente")
+const contenedorPreguntas = document.querySelector("#contenedor-preguntas")
+const cadaPregunta = document.querySelector("#pregunta")
+const opcionRespuestas = document.querySelector("#btn-respuestas")
+const contenedorGrafico = document.querySelector("#contenedor-grafico")
+const contenedorResultado = document.querySelector("#contenedor-resultados")
