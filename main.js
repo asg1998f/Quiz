4c7ad8f4d7e48ccaec55696bd34cc916d3d03508
asg1998f <asg1998f@gmail.com>
2024-11-14 15:51:42 +0100

    correcciones + función reset

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ const containerResults = document.querySelector("#contenedor-results")
 let questions = []
 let currentQuestionIndex = 0;
 
-const getQuestions = async () => {
+/* const getQuestions = async () => {
     const res = await axios.get(
-        "https://opentdb.com/api.php?amount=10&type=multiple&encode=base64"
+        "https://opentdb.com/api.php?amount=10&category=22&difficulty=easy&type=multiple"
     );
     questions = res.data.results.map((questionObj) => {
         let answers = questionObj.incorrect_answers.map((answer) => {
@@ -23,7 +23,31 @@ const getQuestions = async () => {
             answers,
         };
     });
+    console.log(questions)
+}; */
+
+const getQuestions = async () => {
+    try {
+        const res = await axios.get(
+            "https://opentdb.com/api.php?amount=10&category=22&difficulty=easy&type=multiple"
+        );
+        console.log(res.data);  
+        
+        questions = res.data.results.map((questionObj) => {
+            let answers = questionObj.incorrect_answers.map((answer) => {
+                return { text: answer, correct: false };
+            });
+            answers.push({ text: questionObj.correct_answer, correct: true });
+            return {
+                question: questionObj.question,
+                answers,
+            };
+        });
+    } catch (error) {
+        console.error("Error al obtener las preguntas", error);
+    }
 };
+
 
 
 
@@ -35,7 +59,7 @@ function setStatusClass(element){
     }
 };
 
-function selectAnswer(){
+ function selectAnswer(){
     Array.from(optionAnswers.children).forEach((button)=>{
         setStatusClass(button);
     });
@@ -45,9 +69,19 @@ function selectAnswer(){
         beginButton.innerText = "Restart"
         beginButton.classList.remove("hide")
     }
-}
+ }
+  
+    
+    function setStatusClass(button) {
+        if (button.dataset.correct) {
+            button.classList.add("correct");
+        } else {
+            button.classList.add("wrong");
+        }
+    }
+    
 
-function showQuestion (question) {
+    function showQuestion (question) {
     eachQuestion.innerText = question.question;
     optionAnswers.innerHTML="";
     question.answers.forEach((answer)=>{
@@ -59,11 +93,18 @@ function showQuestion (question) {
         button.addEventListener("click",selectAnswer);
         optionAnswers.append(button);
     });
-};
+}; 
+
+
 
 function setNextQuestion(){
     resetState();
     showQuestion(questions[currentQuestionIndex])
+}
+
+function resetState() {
+    nextButton.classList.add("hide");
+    optionAnswers.classList.remove("hide");
 }
 
 async function startGame(){
@@ -74,12 +115,12 @@ async function startGame(){
 }
 beginButton.addEventListener("click",startGame);
 
-nextButton.addEventListener("click",() => {
+nextButton.addEventListener("click", () => {
     currentQuestionIndex++;
-if(currentQuestionIndex<questions.length){
-    setNextQuestion();
-}else{
-    containerQuestions.classList.add("hide");
-    containerResults.classList.remove("hide");
-}
+    if (currentQuestionIndex < questions.length) {
+        setNextQuestion(); 
+    } else {
+        containerQuestions.classList.add("hide");
+        containerResults.classList.remove("hide");
+    }
 });
